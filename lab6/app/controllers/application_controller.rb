429cfb82bca9976967e5508bc24464421a820e4a
AndrukhivBohdan <114477382+AndrukhivBohdan@users.noreply.github.com>
2025-12-10 14:19:43 +0200

    Update application_controller.rb

--- lab6/app/controllers/application_controller.rb
+++ lab6/app/controllers/application_controller.rb
@@ -3,15 +3,14 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-    # Після успішної реєстрації/логіну
+
   def after_sign_in_path_for(resource)
-    landing_pages_path   # або назва шляху до твоєї landing_page
+    landing_pages_path  
   end
 
-  # Якщо хочеш окремо після sign up
   def after_sign_up_path_for(resource)
     landing_pages_path
   end
-  # Changes to the importmap will invalidate the etag for HTML responses
+    
   stale_when_importmap_changes
 end
